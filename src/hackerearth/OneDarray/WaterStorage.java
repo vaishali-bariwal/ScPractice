40bce28071ef9454ccf513cea43a5af7e7d009d2
Sumit Murari <murari.sumit@outlook.com>
2021-07-22 03:57:19 +0530

    Water storage

--- src/hackerearth/OneDarray/WaterStorage.java
+++ src/hackerearth/OneDarray/WaterStorage.java
@@ -1,10 +1,30 @@
 package hackerearth.OneDarray;
 
 public class WaterStorage {
+	 public static int trap(final int[] a) {
+	        
+	        int n = a.length;
+	        int[] prefix = new int[n];// to find max hiegth in right side
+	        prefix[n-1] = a[n-1];
+	        for(int i = n-2;i >= 0;i--)
+	        {
+	            prefix[i] = Math.max(a[i],prefix[i+1]);
+	        }
+	        int lmax = 0,ans= 0;
+	        for(int i = 0 ; i < n;i++)
+	        {
+	            lmax = Math.max(a[i],lmax);
+	            ans += Math.min(lmax, prefix[i]) - a[i];
+	            
+	        }
+	        return ans;
+	    }
+
 	
 	public static void main(String args [])
 	{
-		System.out.println("Hiiii");
+		int[] a = {0,3,4,0,3,2,1,3,};
+		System.out.println(trap(a ));
 	}
 
 }
